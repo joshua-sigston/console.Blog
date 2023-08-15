0cd67b1c219f7ddfcddb299668ced9d34bb8f6b4
Joshua Sigston <joshua.sigston@outlook.com>
2023-08-15 15:48:35 -0600

    try dif connect code

--- app.js
+++ app.js
@@ -13,18 +13,46 @@ const PORT = process.env.PORT || 3000;
 
 // connect DB
 const MongoStore = require('connect-mongo');
-const connectDB = require('./server/config/db');
-connectDB();
+var mongoose        =   require('mongoose');
+// const connectDB = require('./server/config/db');
+// connectDB();
+
+// app.use(session({
+//   secret: 'keyboard cate',
+//   resave: false,
+//   saveUninitialized: false,
+//   store: MongoStore.create({
+//     mongoUrl: process.env.MONOGDB_URI,
+//     touchAfter: 24 * 3600 
+//   })
+// }));
+
+mongoose.connect(process.env.MONOGDB_URI,{useNewUrlParser: true, useUnifiedTopology: true}).catch(error => console.log("App.js mongoose.connect error",error));
+
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function(){
+    console.log("App is connected to DB", db.name)
+});
+mongoose.Promise = global.Promise;
 
 app.use(session({
-  secret: 'keyboard cate',
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({
-    mongoUrl: process.env.MONOGDB_URI,
-    touchAfter: 24 * 3600 
-  })
+    secret: 'Your secret here',
+    saveUninitialized: false,
+    resave: false,
+    store: MongoStore.create({
+      client: mongoose.connection.getClient(), //  as per the new versioning use client here and you can pass either the link or the same mongoose connection
+      //Use above client if you want to use an existing connection
+
+
+      // mongoUrl:"mongodb://localhost:27017/DBNAME",
+      // or use this mongoUrl if you are passing the database URL straight away
+
+      ttl: 1 * 6 * 60 * 60,  //ttl: 14 * 24 * 60 * 60, //days * hours * minutes * seconds
+      autoRemove: 'native' // Default
+    })
 }));
+
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(cookieParser());
